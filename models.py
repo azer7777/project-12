505e55c8be9040b7da14317c40d2de20b5546053
azer7777 <azertyuio123a77@gmail.com>
2023-10-18 20:40:48 +0200

    customers db

---
+++ models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+# Define the Customer class
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    phone = Column(String)
+    company_name = Column(String)
+    creation_date = Column(Date, nullable=False)
+    last_contact_date = Column(Date)
+    sales_contact = Column(String)
+    contracts = relationship('Contract', back_populates='customer')
